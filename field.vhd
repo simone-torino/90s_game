168b6ca43266c87fd83039af3c2402ff3ec62de2
Simone Torino <simone.torino.12@gmail.com>
2023-01-27 10:14:24 +0100

    Added comments

--- field.vhd
+++ field.vhd
@@ -8,28 +8,31 @@ ENTITY field IS
         xscan, yscan : IN INTEGER;
         right_limit, left_limit, top_limit, bottom_limit : BUFFER INTEGER;
         flag : OUT STD_LOGIC
-        --red, green, blue : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
     );
 END field;
 
 ARCHITECTURE behavior OF field IS
-    CONSTANT offset : INTEGER := 30;
-    CONSTANT offset_top : INTEGER := 60;
-    CONSTANT thickness : INTEGER := 5;
+    CONSTANT offset : INTEGER := 30; --Distance from display edge
+    CONSTANT offset_top : INTEGER := 60; --Distance from the top is higher to let space for text
+    CONSTANT thickness : INTEGER := 5; --Thickness of the field edges
     CONSTANT y_max : INTEGER := 480;
     CONSTANT x_max : INTEGER := 640;
 
 BEGIN
+    --Field is drawn by overlapping a black rectangle over a slightly larger white rectangle
     draw_field : PROCESS (clk, rstn)
     BEGIN
         IF (rstn = '0') THEN
             flag <= '0';
         ELSIF (clk'event AND clk = '1') THEN
             IF (en = '1') THEN
+                --White rectangle dimensions define the outer edge
                 IF (xscan >= offset AND xscan <= x_max - offset AND yscan >= offset_top AND yscan <= y_max - offset) THEN
                     flag <= '1';
+                    --Black rectangle dimensions define the inner edge
                     IF (xscan >= left_limit AND xscan <= right_limit AND yscan >= top_limit AND yscan <= bottom_limit) THEN
                         flag <= '0';
+                        --Mid vertical line divides the field in half.
                         IF (xscan >= left_limit + ((right_limit - left_limit)/2) - 1 AND xscan <= left_limit + ((right_limit - left_limit)/2) + 1 AND yscan >= top_limit AND yscan <= bottom_limit) THEN
                             flag <= '1';
                         END IF;
